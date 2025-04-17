10ae83b7a98e3d657ed608eddcd8f5f604dbd5dd
Israel Ekpo <isekpo@microsoft.com>
2025-04-17 14:53:05 -0400

    saving changes

--- maintenance_scripts/azure_blob_store.sh
+++ maintenance_scripts/azure_blob_store.sh
@@ -3,3 +3,10 @@
 source ./environment.sh
 
 az storage account update -g ${AZURE_RESOURCE_GROUP} --name ${STORAGE_ACCOUNT}  --allow-shared-key-access true
+
+az storage account update \
+  --name ${STORAGE_ACCOUNT} \
+  --resource-group ${AZURE_RESOURCE_GROUP} \
+  --public-network-access Enabled \
+  --default-action Allow
+
