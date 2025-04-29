f03814f8ac31054750a9a3578335d77b0012cf4a
Israel Ekpo <isekpo@microsoft.com>
2025-04-29 12:55:55 -0400

    saving changes

---
+++ .confluent-environment.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Confluent Cluster and Environment Details for CLI setup
+export CONFLUENT_ORGANIZATION_ID="put your variable value here"
+export CONFLUENT_ENVIRONMENT_ID="put your variable value here"
+
+# Azure Service Principal Credentials
+export SERVICE_PRINCIPAL_CLIENT_ID="put your variable value here"
+export SERVICE_PRINCIPAL_CLIENT_SECRET="put your variable value here"
+export SERVICE_PRINCIPAL_TENANT_ID="put your variable value here"
+export SERVICE_PRINCIPAL_SUBSCRIPTION_ID="put your variable value here"
+
+# Terraform Configuration
+export ARM_SUBSCRIPTION_ID="put your variable value here"
+export ARM_CLIENT_ID="put your variable value here"
+export ARM_CLIENT_SECRET="put your variable value here"
+export ARM_TENANT_ID="put your variable value here"
+
+# Kafka Cluster Details
+export KAFKA_CLUSTER_ID="put your variable value here"
+export KAFKA_API_KEY="put your variable value here"
+export KAFKA_API_SECRET="put your variable value here"
+
+# Flink Compute Pool Details
+export FLINK_COMPUTE_POOL_ID="put your variable value here"
+export FLINK_COMPUTE_POOL_NAME="put your variable value here"
+export FLINK_COMPUTE_POOL_REGION="put your variable value here"
+
+# Azure Resource Group for Resources
+export AZURE_RESOURCE_GROUP="put your variable value here"
+
+# Azure Cosmos DB Credentials
+export AZURE_COSMOS_DB_ACCOUNT_NAME="put your variable value here"
+export AZURE_COSMOS_DB_ACCOUNT_KEY="put your variable value here"
+export AZURE_COSMOS_DB_DATABASE_NAME="put your variable value here"
+
+# Azure AI Search Credentials
+export AZURE_SEARCH_SERVICE_NAME="put your variable value here"
+export AZURE_SEARCH_API_KEY="put your variable value here"
+
+# Azure Storage Account Credentials
+export AZURE_STORAGE_ACCOUNT_NAME="put your variable value here"
+export AZURE_STORAGE_ACCOUNT_KEY="put your variable value here"
+
+# Azure Redis Cache Credentials
+export REDIS_HOSTNAME="put your variable value here"
+export REDIS_PRIMARY_ACCESS_KEY="put your variable value here"
